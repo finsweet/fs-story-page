9bc2dce7b48180b252ecc1d7a5a36aea3ef14911
PeterEbili <eebilipeter2016@gmail.com>
2022-01-04 11:37:36 +0100

    assets and scripts

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+import '../scss/style.scss'
+//import Timeline from './components/Timeline'
+import Timeline from './components/DomTimeline'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Initial HMR Setup
+if (module.hot) {
+    module.hot.accept()
+
+    module.hot.dispose(() => {
+        window.assets = timeline.assets
+        timeline.renderer.domElement.removeEventListener('wheel', timeline.scroll)
+        removeEventListener('resize', timeline.resize)
+        timeline.renderer.domElement.removeEventListener('mousedown', timeline.mouseDown)
+        timeline.renderer.domElement.removeEventListener('mouseup', timeline.mouseUp)
+        removeEventListener('mousemove', timeline.mouseMove)
+        document.querySelector('canvas').remove()
+        timeline.renderer.forceContextLoss()
+        timeline.renderer.context = null
+        timeline.renderer.domElement = null
+        timeline.renderer = null
+        cancelAnimationFrame(timeline.animationId)
+
+        timeline.gesture.destroy()
+    })
+}
+
+    const timeline = new Timeline()
+    window.timeline = timeline
+})
